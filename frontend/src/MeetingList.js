984e30152174a2d3f9ce39227d786464de9903b5
Cristiana Jipa <jipacristiana@gmail.com>
2022-01-31 14:29:33 +0200

    React Changes 2

--- frontend/src/MeetingList.js
+++ frontend/src/MeetingList.js
@@ -9,6 +9,8 @@ import EditIcon from '@material-ui/icons/Edit';
 import PersonIcon from '@material-ui/icons/Person';
 import {meetRoute} from './ApiRoutes.js';
 import { useNavigate } from 'react-router-dom';
+import './MeetingList.css';
+
 
 export default function MeetingList(){
     
@@ -37,34 +39,34 @@ export default function MeetingList(){
             <h2>See  your following meetings, add one, edit and delete.</h2>
 
             <br/>
-
+            
             <TableContainer component={Paper}>
                 <Table aria-label="simple table">
                     <TableHead>
                         <TableRow>
                             <TableCell>Meeting Id</TableCell>
-                            <TableCell align="right">Meeting Description</TableCell>
-                            <TableCell align="right">Meeting URL</TableCell>
-                            <TableCell align="right">Meeting Date</TableCell>
-                            <TableCell align="right">Participants</TableCell>
-                            <TableCell align="right">Actions</TableCell>
+                            <TableCell align="center">Meeting Description</TableCell>
+                            <TableCell align="center">Meeting URL</TableCell>
+                            <TableCell align="center">Meeting Date</TableCell>
+                            <TableCell align="center">Participants</TableCell>
+                            <TableCell align="center">Edit or Delete</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {rows.map((row, index) => (
                             <TableRow key={row.MeetingId}>
-                                <TableCell component="th" scope="row">
+                                <TableCell component="th" scope="row" align="center">
                                     {row.MeetingId}
                                 </TableCell>
-                                <TableCell align='right'>{row.MeetingDescription}</TableCell>
-                                <TableCell align='right'>{row.MeetingURL}</TableCell>
-                                <TableCell align='right'>{row.MeetingDate}</TableCell>
-                                <TableCell align='right'>
+                                <TableCell align='center'>{row.MeetingDescription}</TableCell>
+                                <TableCell align='center'>{row.MeetingURL}</TableCell>
+                                <TableCell align='center'>{row.MeetingDate}</TableCell>
+                                <TableCell align='center'>
                                     <IconButton onClick={() => navigate(`/Participant/${row.MeetingId}`)}>
                                         <PersonIcon />
                                     </IconButton>
                                 </TableCell>
-                                <TableCell align="right">
+                                <TableCell align="center">
                                     <IconButton onClick={() => navigate(`/AddMeeting/${row.MeetingId}`)}>
                                         <EditIcon color="primary" />
                                     </IconButton>
@@ -77,11 +79,12 @@ export default function MeetingList(){
                     </TableBody>
                 </Table>
             </TableContainer>
+            
             <br></br>
                      <div>
                           <Button
                            variant='contained'
-                            color="dark"
+                            color="inherit"
                          startIcon={<AddIcon />}
                          onClick={() => {navigate("AddMeeting")}}
                              >
@@ -92,6 +95,8 @@ export default function MeetingList(){
                      <br></br>
                      <div>
                           <Button
+                          variant='contained'
+                          color="primary"
                            id = 'export'
                          startIcon={<SendSharpIcon />}
                          
@@ -103,6 +108,8 @@ export default function MeetingList(){
                      <br></br>
                      <div>
                           <Button
+                          variant='contained'
+                          color="primary"
                            id= 'import'
                          startIcon={<ImportExportIcon />}
                          
